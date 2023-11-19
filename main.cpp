5ad1969a1a32c5c529a4cd930c9b569bd2e5ae22
Kanezoh <asfaltmanta114@gmail.com>
2023-11-19 09:32:52 +0900

    テンプレートを変更

--- main.cpp
+++ main.cpp
@@ -1,28 +1,31 @@
 #include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; (i) < (int)(n); ++ (i))
+#define REP3(i, m, n) for (int i = (m); (i) < (int)(n); ++ (i))
+#define REP_R(i, n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
+#define REP3R(i, m, n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
+#define ALL(x) std::begin(x), std::end(x)
 using namespace std;
-#define dump(x) cout << #x << " = " << (x) << endl;
-#define REP(i, n) for (ll i = 0; i < (n); i++)
-#define ITOC(n) (char)'0' + n
-#define KETAUNE(keta, num) setw(keta) << setfill('0') << num
-#define SORTVEC(vec) sort(vec.begin(), vec.end())
-#define RSORTVEC(vec) sort(vec.rbegin(), vec.rend())
-#define REVERSEVEC(vec) reverse(vec.begin(), vec.end())
-#define FOREACH(x, a) for (auto &x : (a))
-#define OUT(n) cout << n << endl
-#define SISHAGONYU(X, base) round(X / (double)base) * base
-#define KIRISUTE(X, base) X / base *base
-typedef long long ll;
-// #include <atcoder/all>
-// using namespace atcoder;
 
-// printf("%.3lf", S);
-// 小数で割ったあまり　fmod(1,1.0)
-// 末尾3文字 str.substr(str.length() - 3)
-// 円周率 M_PI
-// 等差数列の和
-// a1: 初項、n: 項数、d: 項差
-// return n * (2 * a1 + (n - 1) * d) /2
+int64_t solve(int n, const vector<int64_t>& a) {
+    // ...
+}
 
 int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  constexpr char endl = '\n';
+
+  // input
+  int n; cin >> n;
+  vector<int64_t> a(n);
+  REP (i, n) {
+      cin >> a[i];
+  }
+
+  // solve
+  auto ans = solve(n, a);
+
+  // output
+  cout << ans << endl;
 }
 
